e409ee8ab25fe55439d92d930e0046eb44cfdb1d
FarmMachine21 <70625605+FarmMachine21@users.noreply.github.com>
2020-10-11 05:39:09 +0900

    v0.2

--- DBP_Git_Push/DBP_Git_Push/Form1.cs
+++ DBP_Git_Push/DBP_Git_Push/Form1.cs
@@ -19,8 +19,8 @@ namespace DBP_Git_Push
 
         private void button1_Click(object sender, EventArgs e)//버튼 클릭시
         {
-            pictureBox1.Image = System.Drawing.Image.FromFile("Chicken_2.jpg");//이미치 출력
-            label1.Text = "치킨";//텍스트 출력
+            //pictureBox1.Image = System.Drawing.Image.FromFile("Chicken_2.jpg");//이미치 출력
+            label1.Text = "피자";//텍스트 출력, 충돌상황 만들기 위해 텍스트 바꿈.
         }
     }
 }
