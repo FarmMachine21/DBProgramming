4fc79cab9e19195e15f2f3b6576c7b9dda832896
FarmMachine21 <70625605+FarmMachine21@users.noreply.github.com>
2020-10-11 05:03:16 +0900

    치킨 이미지가 뜨고 치킨 텍스트가 나오도록 작성.

---
+++ DBP_Git_Push/DBP_Git_Push/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DBP_Git_Push
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)//버튼 클릭시
+        {
+            pictureBox1.Image = System.Drawing.Image.FromFile("Chicken_2.jpg");//이미치 출력
+            label1.Text = "치킨";//텍스트 출력
+        }
+    }
+}
